05da276ec9b2270f96786255af64b4681fdebcb6
Karn Ashutosh Singh <karns7844@gmail.com>
2025-09-17 15:16:36 +0530

    skeleton route

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app =express();
+
+
+app.use(express.json())
+
+
+ app.get("/signup",function(req,res){
+
+
+
+ })
+
+
+app.post("/login",function(req,res){
+
+})
+
+
+app.get("/purchasecouse",function(req,res){
+
+})
+
+app.get("/course-see",function(req,res){
+    
+})
