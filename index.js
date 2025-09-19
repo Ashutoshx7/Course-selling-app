66b25174af30c3dca1849ae549ebf490e2436786
Karn Ashutosh Singh <karns7844@gmail.com>
2025-09-20 04:14:21 +0530

    endpoints

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app =express();
 const { userRouter } = require("./routes/user");
 const {courseRouter}=require ("./routes/course")
+const {adminRouter}= require("./routes/course")
 
 
 
@@ -9,6 +10,7 @@ const {courseRouter}=require ("./routes/course")
 
 app.use("/user", userRouter);
 app.use("/course",courseRouter);
+app.use("/admin",adminRouter)
 
 
 app.use(express.json())
---
+++ routes/admin.js
@@ -0,0 +1,34 @@
+const {Router}=require("express");
+
+const adminRouter=Router();
+
+
+
+adminRouter.post("/signup",function(req,res){
+   
+})
+
+
+
+adminRouter.post("/login",function(req,res){
+
+    res.json({
+        message:"you are loged in "
+    })
+
+})
+
+adminRouter.get("/content",function(req,res){
+
+})
+
+
+adminRouter.delete("/delete",function(req,res){
+
+})
+
+
+
+module.exports=({
+    adminRouter:adminRouter,
+})
